224555c07d032974f1fc0ec36ddd80d42abf7eb7
sergey280680 <sergey280680@mail.ru>
2019-12-05 23:52:24 +0200

    tnt

--- js/main.js
+++ js/main.js
@@ -17,12 +17,14 @@ $(document).ready(function() {
 
 
 // =================  menu burger  ============ //
-     $(".menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#sidebar-wrapper").toggleClass("active");
-        $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
-        $(this).toggleClass("active");
-      });
+//      $(".menu-toggle").click(function(e) {
+//         e.preventDefault();
+//         $("#sidebar-wrapper").toggleClass("active");
+//         $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+//         $(this).toggleClass("active");
+//       });
+//
+//
 
 
 // ====== modal display function ========//
